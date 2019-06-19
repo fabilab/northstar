1c17b85725f24f63b328cf58c60a7cfcc6a94cf4
Fabio Zanini <fabio.zanini@fastmail.fm>
2019-06-18 17:32:10 -0700

    New test ad hoc works

--- tests/test_small.py
+++ tests/test_small.py
@@ -27,4 +27,31 @@ def test_random_small():
         assert(isinstance(nei, list))
         assert(len(nei) <= k)
 
-    print(neis)
+
+def test_adhoc_small():
+    N = 200
+    L = 50
+
+    # Data matrix is random, but has defined neighborhoods
+    matrix = np.ones((L, N), np.float32)
+    matrix[:10, 1] += 2
+    matrix[20:30, 2] -= 2
+    matrix[:10, 40:80] += 2
+    matrix[20:30, 100:130] -= 2
+
+    n_fixed = 3
+    sizes = np.array([10, 30, 5] + [1] * (N - n_fixed))
+    n_pcs = 2
+    k = 5
+    threshold = 100
+
+    neis = compute_graph(
+            matrix, sizes, n_fixed, k, threshold, n_pcs=n_pcs,
+            metric='euclidean',
+            )
+
+    for nei in neis[37:77]:
+        assert(set(nei) & set([1] + list(range(40, 80))) == set(nei))
+    for nei in neis[97:127]:
+        assert(set(nei) & set([2] + list(range(100, 130))) == set(nei))
+
